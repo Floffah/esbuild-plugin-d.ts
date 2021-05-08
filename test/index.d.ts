0a9befec9233283f430fd247b3ff1fdd41d86df3
Floffah <therealfloffah@gmail.com>
2021-05-08 11:44:48 +0100

    remove test outputs from test

--- test/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function someFunction(a: string, b: number): string;
-export declare type SomeType = "someValue" | "anotherValue";
-export declare const someVar: SomeType;
--- test/out.cjs
+++
@@ -1,9 +0,0 @@
-// test/index.ts
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.someVar = exports.someFunction = void 0;
-function someFunction(a, b) {
-  return a + b;
-}
-exports.someFunction = someFunction;
-exports.someVar = "someValue";
