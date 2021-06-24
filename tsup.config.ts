03dfbaa9f47820726f211b42a1bc44fb2df7dc25
Floffah <therealfloffah@gmail.com>
2021-06-24 21:46:41 +0000

    build using tsup

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { Options } from "tsup"
+
+export const tsup: Options = {
+    target: "node12",
+    entryPoints: ["./src/index.ts"],
+    external: ["esbuild", "typescript", "chalk", "tmp"],
+    dts: true,
+    clean: true,
+    sourcemap: true
+};
