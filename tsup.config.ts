1daecac9cb7b7cb27af3a38a07e2452f2df3062d
Floffah <floffah@floffah.dev>
2023-12-11 21:15:01 +0000

    fix tsup formats

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { Options } from "tsup";
 export const tsup: Options = {
     target: "node12",
     entryPoints: ["./src/index.ts"],
+    format: ["cjs", "esm"],
     external: ["esbuild", "typescript", "chalk", "tmp"],
     dts: true,
     clean: true,
