c0a5b77864c455958c0f2158f282bc16127c5948
therealfloffah@gmail.com <therealfloffah@gmail.com>
2024-06-21 15:10:57 +0100

    test: move each test to its own directory

---
+++ tests/basic/basic.test.ts
@@ -0,0 +1,18 @@
+import { dtsPlugin } from "../../dist";
+import { distDir, readOutputFile } from "../_utils";
+import { expect, test } from "bun:test";
+import { build } from "esbuild";
+import { resolve } from "path";
+
+test("Basic config", async () => {
+    const tsconfig = resolve(__dirname, "./tsconfig.json");
+
+    await build({
+        plugins: [dtsPlugin({ tsconfig })],
+        entryPoints: [resolve(__dirname, "./basic.ts")],
+        outdir: distDir,
+        tsconfig,
+    });
+
+    expect(readOutputFile("basic")).toMatchSnapshot();
+});
